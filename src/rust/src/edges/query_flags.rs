23318a3c349e952c9fba58cea2b70763ab0fe384
Frederik Fabricius-Bjerre <frederik@fabriciusbjerre.dk>
2025-09-17 21:09:41 +0200

    Update QueryFlags to encompass all possible types

--- src/rust/src/edges/query_flags.rs
+++ src/rust/src/edges/query_flags.rs
@@ -4,24 +4,28 @@ use bitflags::bitflags;
 bitflags! {
     #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
     pub struct QueryFlags: u16 {
+        // Tail and head is determined by edge orientation.
+
         // Per-side role bits for SPLIT_INDEX placement
         const TAIL_PARENT = 0b0000_0001;
-        const TAIL_CHILD  = 0b0000_0010;
-        const TAIL_UNDIR  = 0b0000_0100;
+        const TAIL_CHILD = 0b0000_0010;
+        const TAIL_UNDIR = 0b0000_0100;
+        const TAIL_UNKNOWN = 0b0100_0000; // for PAG edges
         const HEAD_PARENT = 0b0000_1000;
-        const HEAD_CHILD  = 0b0001_0000;
-        const HEAD_UNDIR  = 0b0010_0000;
+        const HEAD_CHILD = 0b0001_0000;
+        const HEAD_UNDIR = 0b0010_0000;
+        const HEAD_UNKNOWN = 0b1000_0000; // for PAG edges
         
         // Does the edge indicate latent confounding?
-        const LATENT_CONFOUNDING = 0b1000_0000;
+        const LATENT_CONFOUNDING = 0b1000_0000_0000;
 
         // Per-side possible-role bits for queries
-        const HEAD_POSS_CHILD  = 0b0000_1000_0000;
+        const HEAD_POSS_CHILD = 0b0000_1000_0000;
         const HEAD_POSS_PARENT = 0b0001_0000_0000;
         const TAIL_POSS_PARENT = 0b0000_0100_0000;
-        const TAIL_POSS_CHILD  = 0b0010_0000_0000;
+        const TAIL_POSS_CHILD = 0b0010_0000_0000;
 
         // Extra semantics (e.g., d/m-sep traversal tweaks)
-        const TRAVERSABLE_WHEN_CONDITIONED = 0b0100_0000;
+        const TRAVERSABLE_WHEN_CONDITIONED = 0b0100_0000_0000;
     }
 }
