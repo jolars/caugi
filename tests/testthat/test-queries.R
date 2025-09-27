8381a23d6e54a8a1c338b942e5fc54f78574d411
Frederik Fabricius-Bjerre <frederik@fabriciusbjerre.dk>
2025-09-27 14:29:33 +0200

    Test is_dag and is_pdag

--- tests/testthat/test-queries.R
+++ tests/testthat/test-queries.R
@@ -13,12 +13,59 @@ test_that("acyclicity check works", {
   cg <- cg |> set_edges(C %---% A)
   expect_true(is_acyclic(cg))
 })
+
 test_that("is_acyclic forces check when requested", {
   cg <- caugi_graph(A %-->% B, B %-->% C, C %-->% D, class = "DAG")
 
-  # we do not think it is possible to introduce cycles in a caugi without
-  # the package throwing errors, so force_checking is more a feature that
-  # is there for peace of mind than a necessity
   expect_true(is_acyclic(cg))
   expect_true(is_acyclic(cg, force_check = TRUE))
+})
+
+test_that("query builds", {
+  cg <- caugi_graph(A %-->% B, B %-->% C, C %---% D, class = "PDAG")
+  expect_true(cg@built)
+
+  cg <- cg |> add_edges(D %-->% E)
+  expect_false(cg@built)
+
+  expect_true(is_acyclic(cg))
+
+  # now it should be build
+  expect_true(cg@built)
+})
+
+# ──────────────────────────────────────────────────────────────────────────────
+# ────────────────────────────── Is it <type>? ─────────────────────────────────
+# ──────────────────────────────────────────────────────────────────────────────
+
+test_that("is_dag works", {
+  cg <- caugi_graph(A %-->% B, B %-->% C, C %-->% D, class = "DAG")
+  expect_true(is_dag(cg))
+
+  cg <- caugi_graph(A %-->% B, B %-->% C, C %---% D, class = "PDAG")
+  expect_false(is_dag(cg))
+
+  cg <- caugi_graph(A %-->% B, B %-->% C, C %---% D, class = "Unknown")
+  expect_false(is_dag(cg))
+
+  cg <- cg |> set_edges(C %-->% D)
+  expect_true(is_dag(cg))
+})
+
+test_that("is_pdag works", {
+  cg <- caugi_graph(A %-->% B, B %-->% C, C %---% D, class = "PDAG")
+  expect_true(is_pdag(cg))
+
+  cg <- caugi_graph(A %-->% B, B %-->% C, C %-->% D, class = "DAG")
+  expect_true(is_pdag(cg))
+
+  cg <- caugi_graph(A %-->% B, B %-->% C, C %---% D, class = "Unknown")
+  expect_true(is_pdag(cg))
+
+  cg <- cg |> set_edges(C %o->% D)
+  expect_false(is_pdag(cg))
+
+  cg <- caugi_graph(A %-->% B, B %-->% C, C %o->% D, class = "Unknown")
+
+  expect_false(is_pdag(cg))
 })
