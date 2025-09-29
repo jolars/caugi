e8a800c103ec6b518dd5545ca2067c25bb9d00bf
Frederik Fabricius-Bjerre <frederik@fabriciusbjerre.dk>
2025-09-29 07:37:48 +0200

    Move methods to its own file

--- R/caugi_graph.R
+++ R/caugi_graph.R
@@ -279,33 +279,3 @@ caugi_graph <- S7::new_class(
   e$class <- class
   e
 }
-
-# ──────────────────────────────────────────────────────────────────────────────
-# ───────────────────────────────── methods ────────────────────────────────────
-# ──────────────────────────────────────────────────────────────────────────────
-
-#' Length of a `caugi_graph`
-#'
-#' @description Returns the number of nodes in the graph.
-#'
-#' @param cg A `caugi_graph` object.
-#'
-#' @name length
-#' @returns An integer representing the number of nodes.
-#' @export
-S7::method(length, caugi_graph) <- function(x) {
-  nrow(x@nodes)
-}
-
-#' Print a `caugi_graph`
-#'
-#' @param cg A `caugi_graph` object.
-#' @param ... Not used.
-#'
-#' @name print
-#' @export
-S7::method(print, caugi_graph) <- function(cg, ...) {
-  print(cg@nodes)
-  print(cg@edges)
-  invisible(cg)
-}
