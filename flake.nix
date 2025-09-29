5e94b3bb6d8b21a5b92c7710b884bd8046f1b608
Johan Larsson <jolars@posteo.com>
2025-09-29 15:29:21 +0200

    Add nix development setup
    Setup direnv and a flake to make development on nix easier.
    
    Add nix development setup
    
    Setup direnv and a flake to make development on nix easier.

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.bashInteractive
+            (pkgs.rWrapper.override {
+              packages = with pkgs.rPackages; [
+                cpp11
+                decor
+                devtools
+                dplyr
+                graph
+                igraph
+                knitr
+                languageserver
+                Matrix
+                Matrix
+                pcalg
+                processx
+                rextendr
+                Rgraphviz
+                rhub
+                rlang
+                rmarkdown
+                S7
+                testthat
+                tibble
+                tidyr
+                tidyverse
+                usethis
+              ];
+            })
+          ];
+        };
+      }
+    );
+}
