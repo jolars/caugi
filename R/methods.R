8a8677c2cde2a03c408af4ad69324f29afdabcb9
Frederik Fabricius-Bjerre <frederik@fabriciusbjerre.dk>
2025-10-04 11:04:20 +0200

    Fix some small doc errors

--- R/methods.R
+++ R/methods.R
@@ -6,7 +6,7 @@
 #'
 #' @description Returns the number of nodes in the graph.
 #'
-#' @param cg A `caugi_graph` object.
+#' @param x A `caugi_graph` object.
 #'
 #' @name length
 #' @returns An integer representing the number of nodes.
@@ -23,7 +23,7 @@ S7::method(length, caugi_graph) <- function(x) {
 #' @name print
 #' @export
 S7::method(print, caugi_graph) <- function(x, ...) {
-  print(cg@nodes)
-  print(cg@edges)
-  invisible(cg)
+  print(x@nodes)
+  print(x@edges)
+  invisible(x)
 }
