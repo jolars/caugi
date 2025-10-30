fd8a2ce8ba1c0c9b264fbf4adb08e3e657e8d615
Frederik Fabricius-Bjerre <frederik@fabriciusbjerre.dk>
2025-10-30 16:42:20 +0100

    Add normalize = TRUE to metrics as well as building the graph before applying metric

--- R/metrics.R
+++ R/metrics.R
@@ -25,6 +25,8 @@ shd <- function(cg1, cg2, normalized = FALSE) {
   is_caugi(cg1, throw_error = TRUE)
   is_caugi(cg2, throw_error = TRUE)
   same_nodes(cg1, cg2, throw_error = TRUE)
+  build(cg1)
+  build(cg2)
   out_lst <- shd_of_ptrs(cg1@ptr, cg1@nodes$name, cg2@ptr, cg2@nodes$name)
   out <- if (normalized) out_lst$normalized else out_lst$count
   out
@@ -49,6 +51,8 @@ hd <- function(cg1, cg2, normalized = FALSE) {
   is_caugi(cg1, throw_error = TRUE)
   is_caugi(cg2, throw_error = TRUE)
   same_nodes(cg1, cg2, throw_error = TRUE)
+  build(cg1)
+  build(cg2)
   out_lst <- hd_of_ptrs(cg1@ptr, cg2@ptr)
   out <- if (normalized) out_lst$normalized else out_lst$count
   out
@@ -63,6 +67,8 @@ hd <- function(cg1, cg2, normalized = FALSE) {
 #' @param guess A `caugi_graph` object.
 #' @param type A character string specifying the type of AID to compute.
 #' Options are `"oset"` (default), `"ancestor"`, and `"parent"`.
+#' @param normalized Logical; if `TRUE`, returns the normalized AID. If `FALSE`,
+#' returns the count.
 #'
 #' @returns A list containing the score (normalized) and the count.
 #'
@@ -70,11 +76,14 @@ hd <- function(cg1, cg2, normalized = FALSE) {
 #' @concept metrics
 #'
 #' @export
-aid <- function(truth, guess, type = c("oset", "ancestor", "parent")) {
+aid <- function(truth, guess, type = c("oset", "ancestor", "parent"),
+                normalized = TRUE) {
   type <- match.arg(type)
   is_caugi(truth, throw_error = TRUE)
   is_caugi(guess, throw_error = TRUE)
   same_nodes(truth, guess, throw_error = TRUE)
+  build(truth)
+  build(guess)
 
   res <- switch(type,
     oset = oset_aid_of_ptrs(
@@ -96,5 +105,9 @@ aid <- function(truth, guess, type = c("oset", "ancestor", "parent")) {
       guess@nodes$name
     )
   )
-  res
+  if (normalized) {
+    res$score
+  } else {
+    res$count
+  }
 }
