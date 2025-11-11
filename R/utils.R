fc8decfbb6af6914b5e8f3b9256a4883addde854
Frederik Fabricius-Bjerre <frederik@fabriciusbjerre.dk>
2025-11-11 15:19:49 +0100

    New R/utils.R file with .set_names function to remove dependency on stats::setNames

---
+++ R/utils.R
@@ -0,0 +1,20 @@
+# ──────────────────────────────────────────────────────────────────────────────
+# ──────────────────────────── Utility functions ───────────────────────────────
+# ──────────────────────────────────────────────────────────────────────────────
+
+#' @title Set names to an object
+#'
+#' @description
+#' Only made to avoid using `stats::setNames`, so that the `stats` package is not
+#' a dependency.
+#'
+#' @param object An R object to which names are to be assigned.
+#' @param nm A character vector of names to assign to the object.
+#'
+#' @returns The input object with the assigned names.
+#'
+#' @keywords internal
+.set_names <- function(object = nm, nm) {
+  names(object) <- nm
+  object
+}
