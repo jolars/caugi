0c91faac8327e2a92363a39bb723ed34360b2f82
Frederik Fabricius-Bjerre <frederik@fabriciusbjerre.dk>
2025-11-11 15:39:49 +0100

    Change from using tibbles to data.table for edge operators

--- R/edge_operators.R
+++ R/edge_operators.R
@@ -12,14 +12,13 @@
 #' Can be a symbol, string, number, `c(...)`, or a combination using `+`.
 #' @param glyph A string representing the edge glyph (e.g., `"-->"`, `"<->"`).
 #'
-#' @returns A tibble with columns `from`, `to`, and `edge`.
+#' @returns A `data.table` with columns `from`, `to`, and `edge`.
 #'
 #' @keywords internal
 .edge_spec <- function(from_sym, to_expr, glyph) {
   from <- deparse1(from_sym)
-  tos <- .expand_targets(to_expr)
-  tibble::tibble(from = from, to = tos, edge = glyph) |>
-    structure(class = c("caugi_edge_spec", "tbl_df", "tbl", "data.frame"))
+  to <- .expand_targets(to_expr)
+  .edge_constructor(from = from, edge = glyph, to = to)
 }
 
 #' @title Helper to expand the right-hand side of an edge specification
@@ -55,7 +54,9 @@
   if (is.numeric(expr) && length(expr) == 1L) {
     return(as.character(expr))
   }
-  stop("Unsupported right-hand side in edge operator: ", deparse(expr), call. = FALSE)
+  stop("Unsupported right-hand side in edge operator: ", deparse(expr),
+    call. = FALSE
+  )
 }
 
 
@@ -67,24 +68,42 @@
 #' @param lhs The left-hand side node expression.
 #' @param rhs The right-hand side node expression.
 #'
-#' @returns A tibble with columns `from`, `to`, and `edge`.
+#' @returns A `data.table` with columns `from`, `to`, and `edge`.
+#'
+#' @name edge_operators
 #'
 #' @keywords internal
 `%-->%` <- function(lhs, rhs) {
   .edge_spec(substitute(lhs), substitute(rhs), "-->")
 }
-`%<->%` <- function(lhs, rhs) {
-  .edge_spec(substitute(lhs), substitute(rhs), "<->")
-}
+
+#' @rdname edge_operators
+#' @keywords internal
 `%---%` <- function(lhs, rhs) {
   .edge_spec(substitute(lhs), substitute(rhs), "---")
 }
+
+
+#' @rdname edge_operators
+#' @keywords internal
+`%<->%` <- function(lhs, rhs) {
+  .edge_spec(substitute(lhs), substitute(rhs), "<->")
+}
+
+#' @rdname edge_operators
+#' @keywords internal
 `%o-o%` <- function(lhs, rhs) {
   .edge_spec(substitute(lhs), substitute(rhs), "o-o")
 }
+
+#' @rdname edge_operators
+#' @keywords internal
 `%--o%` <- function(lhs, rhs) {
   .edge_spec(substitute(lhs), substitute(rhs), "--o")
 }
+
+#' @rdname edge_operators
+#' @keywords internal
 `%o->%` <- function(lhs, rhs) {
   .edge_spec(substitute(lhs), substitute(rhs), "o->")
 }
